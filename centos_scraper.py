73027ef80c67e6e2ae22ed0d65deba3a26a16081
Celine Keisja <celinekeisja@gmail.com>
2019-10-30 15:18:56 +0800

    Updated the .py file

--- centos_scraper.py
+++ centos_scraper.py
@@ -1,37 +1,43 @@
 from requests import get
 from bs4 import BeautifulSoup
-import re
 import csv
-# filename,download_link,filesize
 
 
-def recr(files):
+def recr(url):
+    """List files from the directory in a csv file recursively."""
+    response = get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
     table = soup.find('table')
     rows = table.find_all('tr')
-    if 'str'.endswith('/'):
-        csv_file(rows)
-        'list files'
-    else:
-        recr()
+    for row in rows[3:-1]:
+        tds = row.find_all('td')
+        filename = tds[1].text
+        if filename.strip().endswith('/'):
+            recr(url+filename.strip())
+        else:
+            d = {"filename": filename, "download_link": url+tds[1].a["href"], "filesize": tds[3].text}
+            csv_append(d)
 
 
-def csv_file(rows):
-    with open(f'test.csv', 'a', newline='') as csv_file:
+def csv_file_headers():
+    """Write the csv file headers."""
+    with open(f'test.csv', 'w+', newline='') as csv_file:
         field_n = ['filename', 'download_link', 'filesize']
         writer = csv.DictWriter(csv_file, fieldnames=field_n)
         writer.writeheader()
-        for row in rows[3:-1]:
-            tds = row.find_all('td')
-            d = {"filename": tds[1].text, "download_link": tds[1].href, "filesize": tds[3].text}
-            print(d)
-            writer.writerow(d)
-    return 'here'
+    return ''
+
+
+def csv_append(d):
+    """Append the encountered files in the existing csv file."""
+    with open('test.csv', 'a', newline='') as csv_file:
+        field_n = ['filename', 'download_link', 'filesize']
+        writer = csv.DictWriter(csv_file, fieldnames=field_n)
+        writer.writerow(d)
+    return ''
 
 
 if __name__ == "__main__":
     url = 'http://mirror.rise.ph/centos/7/'
-    response = get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    files=[]
-    recr(files)
-    csv_file()
+    csv_file_headers()
+    recr(url)
