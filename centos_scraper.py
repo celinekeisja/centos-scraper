0a3acf37e142e2af3f8e2672470ab7d03cfa309b
Celine Keisja <celinekeisja@gmail.com>
2019-10-30 15:36:23 +0800

    Updated centos_scraper file

--- centos_scraper.py
+++ centos_scraper.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 import csv
 
 
-def recr(url):
-    """List files from the directory in a csv file recursively."""
+def list_files(url):
+    """List files from the directory in a CSV file recursively."""
     response = get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     table = soup.find('table')
@@ -13,31 +13,28 @@ def recr(url):
         tds = row.find_all('td')
         filename = tds[1].text
         if filename.strip().endswith('/'):
-            recr(url+filename.strip())
+            list_files(url+filename.strip())
         else:
             d = {"filename": filename, "download_link": url+tds[1].a["href"], "filesize": tds[3].text}
-            csv_append(d)
+            csv_write(d)
 
 
-def csv_file_headers():
-    """Write the csv file headers."""
-    with open(f'test.csv', 'w+', newline='') as csv_file:
-        field_n = ['filename', 'download_link', 'filesize']
-        writer = csv.DictWriter(csv_file, fieldnames=field_n)
-        writer.writeheader()
-    return ''
-
-
-def csv_append(d):
-    """Append the encountered files in the existing csv file."""
-    with open('test.csv', 'a', newline='') as csv_file:
-        field_n = ['filename', 'download_link', 'filesize']
-        writer = csv.DictWriter(csv_file, fieldnames=field_n)
-        writer.writerow(d)
-    return ''
+def csv_write(c):
+    """CSV writing and appending."""
+    if c == 'header':
+        with open(f'test.csv', 'w+', newline='') as csv_file:
+            field_n = ['filename', 'download_link', 'filesize']
+            writer = csv.DictWriter(csv_file, fieldnames=field_n)
+            writer.writeheader()
+    else:
+        with open('test.csv', 'a', newline='') as csv_file:
+            field_n = ['filename', 'download_link', 'filesize']
+            writer = csv.DictWriter(csv_file, fieldnames=field_n)
+            writer.writerow(c)
+    return 'csv done.'
 
 
 if __name__ == "__main__":
-    url = 'http://mirror.rise.ph/centos/7/'
-    csv_file_headers()
-    recr(url)
+    base_url = 'http://mirror.rise.ph/centos/7/'
+    csv_write('header')
+    list_files(base_url)
